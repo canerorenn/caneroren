c1d2a75095cf676ef08cf578388a23dc0137054c
Caner ÖREN <ccanerorenn@gmail.com>
2020-09-20 19:12:26 +0300

    Head düzenlendi

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,11 @@ function Layout({ children }) {
     <div className="layout">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Caner ÖREN - Front-end Developer" />
+        <meta name="keywords" content="caner, caner ören, front-end developer, front-end, graphic, grafiker, web tasarım, web yazılım,
+            web site, web, internet sitesi, internet, tasarım, tasarımcı, tasarımcı, ören, ören caner, ören" />
+        <meta name="google-site-verification" content="G6qPycyIkMMOUYf7F6w0VoNrsq-1fnWE4amxihMq_fU" />
+        <link rel="shortcut icon" type="image/png" href="/icons/favicon.png" />
         <title>Caner ÖREN</title>
       </Head>
       <div className="container">
