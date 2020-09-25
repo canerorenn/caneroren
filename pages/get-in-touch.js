7c49ac80f4c7be19854da8aadfb03dfdd5f9cf0c
Caner ÖREN <ccanerorenn@gmail.com>
2020-09-25 20:34:45 +0300

    Get in touch oluşturuldu

---
+++ pages/get-in-touch.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+import Button from '../components/Button'
+
+export default function ContactPage() {
+  return (
+    <Layout>
+      <section className="contact">
+        <h1>Contact me</h1>
+      </section>
+    </Layout>
+  )
+}
