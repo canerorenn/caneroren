674a3235de08dc828118109e686502e5708aa430
Caner ÖREN <ccanerorenn@gmail.com>
2020-09-25 20:34:37 +0300

    Entry geliştirildi

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
+import Button from '../components/Button'
+
 export default function HomePage() {
   return (
     <Layout>
-
+      <section className="entry">
+        <h1><strong>Hello,</strong> I'm developing frontend jobs as a freelancer.</h1>
+        <p>You can currently <span>work</span> with me.</p>
+        <Button content="Get in touch" tag="link" href="/get-in-touch" className="button" />
+      </section>
     </Layout>
   )
 }
